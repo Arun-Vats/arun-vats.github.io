02e5f38a6daecaa8220ff02cb7e6cd9550276632
Arun Sharma <77892003+Arun-Vats@users.noreply.github.com>
2023-02-07 16:05:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-const modeSwitch = document.getElementById("mode-switch");
-const icon = modeSwitch.querySelector("i");
-
-modeSwitch.addEventListener("click", function() {
-  document.body.classList.toggle("dark-mode");
-  if (icon.classList.contains("fa-sun-o")) {
-    icon.classList.remove("fa-sun-o");
-    icon.classList.add("fa-moon-o");
-  } else {
-    icon.classList.remove("fa-moon-o");
-    icon.classList.add("fa-sun-o");
-  }
-});
