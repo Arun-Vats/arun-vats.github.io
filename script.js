d7598bc33b20f6859505f5aa4b1d9fade5193681
Arun Sharma <77892003+Arun-Vats@users.noreply.github.com>
2023-07-29 11:31:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,16 @@
       const modal = document.getElementById("modal");
       modal.style.display = "none";
     }
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('nav a');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(link.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
+
   </script>
